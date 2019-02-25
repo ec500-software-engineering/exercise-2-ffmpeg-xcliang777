e2fda5e9c27fb947c4c796eaf8944804f4b846c0
xcliang <xcliang@bu.edu>
2019-02-25 15:35:53 -0500

    Update testffmpeg.py

--- testffmpeg.py
+++ testffmpeg.py
@@ -1,9 +1,10 @@
 import pytest
 from main import re_encode_video
 
-def test_ffmpeg(file):
-	dur = re_encode_video(file)
-	assert dur == pytest.approx(1.)
+@pytest.mark.asyncio
+async def test_ffmpeg(file):
+	dur = await re_encode_video(file)
+	assert dur == pytest.approx(5.)
 
 if __name__ == '__main__':
     pytest.main(['-x',__file__])
