d06a6e83e7387ea9fe5ca6b4a13b58a98c2bbcd9
xcliang <xcliang@bu.edu>
2019-02-25 16:12:22 -0500

    update

--- duration.py
+++ duration.py
@@ -1,5 +1,7 @@
 import subprocess
 import json
+import pytest
+from main import ffmpeg
 
 def test_duration(filein):
 	""" get media metadata """
@@ -11,4 +13,4 @@ def test_duration(filein):
                                	text=True)
 	meta_test = json.loads(meta)
 	duration_test = float(meta_test['streams'][0]['duration'])
-	return duration_test
+	assert duration_test == pytest.approx(5.)
