a56755ce074d5ffa594f41d236e17bcb34a7c448
xcliang <xcliang@bu.edu>
2019-02-25 15:25:33 -0500

    update

---
+++ testffmpeg.py
@@ -0,0 +1,12 @@
+import pytest
+from main import re_encode_video
+
+
+def test_ffmpeg():
+	for file in os.listdir("./"):
+		if file.endswith('.mp4'):
+			dur = re_encode_video(file)
+			assert dur == pytest.approx(5.)
+
+if __name__ == '__main__':
+    pytest.main(['-x',__file__])
